a16fbc4a07603811581418a27c6964a1ecf5c863
mac <macun@yonyou.com>
2019-10-03 18:48:27 +0800

    footer

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,19 +3,22 @@
       <home-header></home-header>
       <home-icon></home-icon>
       <home-recommend :list='list'></home-recommend>
+      <home-footer></home-footer>
   </div>
 </template>
 <script>
   import HomeHeader from './components/Header.vue'
   import HomeIcon from './components/Icon.vue'
   import HomeRecommend from './components/Recommend.vue'
+  import HomeFooter from './components/Footer.vue'
   import axios from 'axios'
   export default {
     name:'Home',
     components:{
       HomeHeader,
       HomeIcon,
-      HomeRecommend
+      HomeRecommend,
+      HomeFooter
     },
     data(){
       return {
