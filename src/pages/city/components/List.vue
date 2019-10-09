b0b2e28e5687e767418395313d4cf57bb8f57ecb
mac <macun@yonyou.com>
2019-10-09 11:31:05 +0800

    deploy

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -57,14 +57,17 @@
         // this.scroll.scrollToElement(element)
         // 暂定解决方法
         // 不用B-Scoll，也可实现。
-        if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-          // alert('手机端')
-          document.body.scrollTop = element.offsetTop
-        } else {
-          // alert('电脑端')
-          document.documentElement.scrollTop = element.offsetTop
-          // console.log(document.body.scrollTop)
-        }
+        // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        //   // alert('手机端')
+        //   document.body.scrollTop = element.offsetTop
+        // } else {
+        //   // alert('电脑端')
+        //   document.documentElement.scrollTop = element.offsetTop
+        //   // console.log(document.body.scrollTop)
+        // }
+        document.body.scrollTop  = element.offsetTop
+        document.documentElement.scrollTop = element.offsetTop
+
       }
       },
       mounted() {
